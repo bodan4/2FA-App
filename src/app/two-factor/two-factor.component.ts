b933fb36669a54c892a38669f449dc8ff21830b2
bodan4 <bcirlig4@gmail.com>
2024-07-01 00:47:03 +0300

    Initial commit

---
+++ src/app/two-factor/two-factor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-two-factor',
+  standalone: true,
+  imports: [],
+  templateUrl: './two-factor.component.html',
+  styleUrl: './two-factor.component.css'
+})
+export class TwoFactorComponent {
+
+}
